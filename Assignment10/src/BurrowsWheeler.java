9da04a8d2374f5f21708391535d08507158795db
FuTu <non>
2018-09-26 11:00:28 -0700

    Burrows Wheeler Compression, fixd error when decoding large file

--- Assignment10/src/BurrowsWheeler.java
+++ Assignment10/src/BurrowsWheeler.java
@@ -6,22 +6,20 @@ public class BurrowsWheeler {
 
     public static void transform() {
         String sb = BinaryStdIn.readString();
-        BinaryStdIn.close();
+//        BinaryStdIn.close();
         CircularSuffixArray csa = new CircularSuffixArray(sb);
-        int first = -1;
         for (int i = 0; i < csa.length(); i++) {
             if (csa.index(i) == 0) {
-                first = i;
+                BinaryStdOut.write(i);
                 break;
             }
         }
-        BinaryStdOut.write(first);
         for (int i = 0; i < csa.length(); i++) {
             int pos = csa.index(i) - 1;
             if (pos < 0) {
                 pos += csa.length();
             }
-            BinaryStdOut.write(sb.charAt(pos));
+            BinaryStdOut.write(sb.charAt(pos), 8);
         }
         BinaryStdOut.close();
     }
@@ -29,12 +27,12 @@ public class BurrowsWheeler {
     public static void inverseTransform() {
         int first = BinaryStdIn.readInt();
         String sb = BinaryStdIn.readString();
-        BinaryStdIn.close();
+//        BinaryStdIn.close();
         int length = sb.length();
         char[] t = sb.toCharArray();
         char[] origin = new char[length];
         int[] next = new int[length];
-        char[] count = new char[R + 1];
+        int[] count = new int[R + 1];
 
         for (int i = 0; i < length; i++) {
             count[t[i] + 1] += 1;
@@ -53,7 +51,7 @@ public class BurrowsWheeler {
         }
 
         for (int i = 0; i < length; i++) {
-            BinaryStdOut.write(origin[first]);
+            BinaryStdOut.write(origin[first], 8);
             first = next[first];
         }
         BinaryStdOut.close();
